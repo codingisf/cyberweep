9fe558390e857956e2ed0f8241f858dd31277f42
Balaji <93272649+codingisf@users.noreply.github.com>
2025-02-20 21:47:01 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,44 +1,68 @@
-
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Footer extends Component {
   render() {
     return (
-        <footer className="py-10 bg-black text-white ">
-          
-        <section className="flex justify-between gap-16 px-auto lg:px-16">
-        <div className="">
-          <ul>
-            <li className="font-semibold text-xg text-violet-600">Pages</li>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-          </ul>
-        </div>
-        <div className="">
-          <ul>
-            <li className="font-semibold text-xg text-violet-600">Social</li>
-            <li>Facebook</li>
-            <li>Instagram</li>
-            <li>Twitter</li>
-            <li>Git Hub</li>
-          </ul>
-        </div>
-        <div className="">
-          <ul>
-            <li className="font-semibold text-xg text-violet-600">Resources</li>
-            <li>Blog</li>
-            <li>Medium article</li>
-            <li>Git hub WorkShop</li>
-          </ul>
-        </div>
+      <footer className="py-10 bg-black text-white ">
+        <section className="flex flex-col lg:flex-row justify-end gap-16 px-auto lg:px-16">
+          <div className="mx-auto">
+            <p className="text-violet-800 font-bold ">About Us</p>
+
+<p className="w-[70%]">
+            We are a forward-thinking tech company specializing in cybersecurity
+            training, web development, and UI/UX design. Our mission is to
+            empower individuals and businesses with secure, scalable, and
+            user-friendly digital solutions. With expertise in cybersecurity,
+            networking, and Linux, we provide hands-on training, innovative web
+            solutions, and seamless user experiences.
+            </p>
+
+            <p className="mt-5">🔒 Secure. Innovate. Elevate.🚀 </p>
+          </div>
+
+<div className="flex gap-16">
+
+          <div className="mx-auto">
+            <ul>
+              <li className="font-semibold text-xg text-violet-600">Pages</li>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div className="">
+            <ul>
+              <li className="font-semibold text-xg text-violet-600">Social</li>
+              <li>Facebook</li>
+              <li>Instagram</li>
+              <li>Twitter</li>
+              <li>Git Hub</li>
+            </ul>
+          </div>
+          <div className="">
+            <ul>
+              <li className="font-semibold text-xg text-violet-600">
+                Resources
+              </li>
+              <li>Blog</li>
+              <li>Medium article</li>
+              <li>Git hub WorkShop</li>
+            </ul>
+          </div>
+          </div>
         </section>
-        <span className='text-center'>
-          <p className='text-violet-600 my-5'>Created by <span className='font-semibold text-white text-lg'> Balaji </span></p>
+        <span className="text-center">
+          <p className="text-violet-600 my-5">
+            Created by{" "}
+            <span className="font-semibold text-white text-lg"> Balaji </span>
+          </p>
         </span>
+
+        <hr className="w-[90%] mx-auto my-5" />
+        <p className="text-center">Copyright © 2025 All Rights Reserved by Cyberweep.</p>
       </footer>
-    )
+    );
   }
 }
 
-export default Footer
+export default Footer;
