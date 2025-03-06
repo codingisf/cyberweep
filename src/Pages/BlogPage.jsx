15975d5eb358052be1e11f4f8ae2f76309188599
Balaji <93272649+codingisf@users.noreply.github.com>
2025-03-06 22:00:34 +0530

    Update BlogPage.jsx

--- src/Pages/BlogPage.jsx
+++ src/Pages/BlogPage.jsx
@@ -7,7 +7,7 @@ function BlogPage() {
       <section className="w-[60%] mx-auto h-full">
         <a className="" href="/Blog1">
         <h1 className="text-2xl text-center font-bold">
-          Top 5 Cybersecurity Threats in 2024 – And How to Stop Them
+          Top 4 Cybersecurity Threats in 2024 – And How to Stop Them
         </h1>
         <p className="text-xl font-bold my-5">Introduction</p>
         <p>
@@ -20,6 +20,24 @@ function BlogPage() {
         </p>
         </a>
         <hr className="w-[100%] mx-auto my-20"/>
+      </section>
+
+
+
+
+      <section className="w-[60%] mx-auto h-full">
+        <a className="" href="/Blog2">
+        <h1 className="text-2xl text-center font-bold">
+        Why Secure Coding is Non-Negotiable for Modern Apps
+        </h1>
+        <p className="text-xl font-bold my-5">Introduction</p>
+        <p>
+        Insecure code isn’t just a bug—it’s a business risk. With 60% of data breaches linked to software vulnerabilities (Verizon DBIR, 2024), secure coding is no longer optional. Let’s explore why it’s critical and how to bake security into your development DNA.
+The High Cost of Cutting Corners
+
+        </p>
+        </a>
+        <hr className="w-[100%] mx-auto mt-20"/>
       </section>
     </section>
   );
