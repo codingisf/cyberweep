35b8dced3d675c10b78d2cbacc37ea3f071f2a8a
Balaji <93272649+codingisf@users.noreply.github.com>
2025-03-06 22:28:34 +0530

    Update Blog2.jsx

--- src/Pages/Blog/Blog2.jsx
+++ src/Pages/Blog/Blog2.jsx
@@ -1,1 +1,26 @@
+import React from 'react'
 
+function Blog2() {
+  return (
+    <section className="bg-black min-h-[100vh] text-white ">
+        <h1 className="text-4xl text-center font-bold py-32">Cyberweeb Blogs</h1>
+        <section className="w-[60%] mx-auto h-full">
+        
+        <h1 className="text-2xl text-center font-bold">
+        Why Secure Coding is Non-Negotiable for Modern Apps
+        </h1>
+        <p className="text-xl font-bold my-5">Introduction</p>
+        <p>
+        Insecure code isn’t just a bug—it’s a business risk. With 60% of data breaches linked to software vulnerabilities (Verizon DBIR, 2024), secure coding is no longer optional. Let’s explore why it’s critical and how to bake security into your development DNA.
+The High Cost of Cutting Corners
+
+        </p>
+      </section>
+
+
+
+    </section>
+  )
+}
+
+export default Blog2
