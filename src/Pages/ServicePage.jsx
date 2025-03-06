cf0f540ae51177ff9420f56b1cdaa17d01366e28
Balaji <93272649+codingisf@users.noreply.github.com>
2025-03-06 20:31:48 +0530

    Update ServicePage.jsx

--- src/Pages/ServicePage.jsx
+++ src/Pages/ServicePage.jsx
@@ -152,7 +152,8 @@ function ServicePage() {
       floated={false}
       shadow={false}
       color="transparent"
-      className="absolute inset-0 m-0 h-full w-full rounded-none bg-[url('/casestudy1.jpg')] bg-cover bg-center"
+     className="absolute inset-0 m-0 h-full w-full rounded-none"
+       style={{ backgroundImage: "url('/casestudy1.jpg')", backgroundSize: "cover", backgroundPosition: "center" }}
     >
       <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50" />
     </CardHeader>
@@ -189,12 +190,14 @@ function ServicePage() {
     shadow={false}
     className="relative grid h-[40rem] mx-auto w-full max-w-[28rem] items-end justify-center overflow-hidden text-center"
   >
-    <CardHeader
-      floated={false}
-      shadow={false}
-      color="transparent"
-      className="absolute inset-0 m-0 h-full w-full rounded-none bg-[url('/casestudy2.jpg')] bg-cover bg-center"
-    >
+<CardHeader
+  floated={false}
+  shadow={false}
+  color="transparent"
+  className="absolute inset-0 m-0 h-full w-full rounded-none"
+  style={{ backgroundImage: "url('/casestudy2.jpg')", backgroundSize: "cover", backgroundPosition: "center" }}
+>
+
       <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50" />
     </CardHeader>
     <CardBody className="relative py-14 px-6 md:px-12">
