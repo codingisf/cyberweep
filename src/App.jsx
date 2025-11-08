3d7234cfc43c3c24951631e24f020493e1b480ea
Balaji <93272649+codingisf@users.noreply.github.com>
2025-11-08 17:12:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import StickyNavbar from "./Components/StickyNavbar";
 import Homepage from "./Pages/Homepage";
 import Blog from './Pages/BlogPage';
-import About from "./Pages/AboutPage"; // Import other pages as needed
-import Contact from "./Pages/ContactusPage"; // Import other pages as needed
-import Service from "./Pages/ServicePage"; // Import other pages as needed
-import Blog1 from "./Pages/Blog/blog1"; // Import other pages as needed
-import Blog2 from "./Pages/Blog/Blog2"; // Import other pages as needed
-import Blog3 from "./Pages/Blog/blog3"; // Import other pages as needed
-import Blog4 from "./Pages/Blog/blog4"; // Import other pages as needed
+import About from "./Pages/AboutPage"; 
+import Contact from "./Pages/ContactusPage"; 
+import Training from "./Pages/TrainingPage"; 
+import Service from "./Pages/ServicePage"; 
+import Blog1 from "./Pages/Blog/blog1"; 
+import Blog2 from "./Pages/Blog/Blog2"; 
+import Blog3 from "./Pages/Blog/blog3"; 
+import Blog4 from "./Pages/Blog/blog4"; 
 
 function App() {
 
@@ -24,6 +25,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/service" element={<Service />} />
+        <Route path="/training" element={<Training />} />
         <Route path="/blog/blog1" element={<Blog1 />} />
         <Route path="/blog/blog2" element={<Blog2 />} />
         <Route path="/blog/blog3" element={<Blog3 />} />
